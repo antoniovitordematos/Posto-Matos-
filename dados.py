d62fa9a16f757c06b997917a27c19351743ccdfc
Antonio Vitor <antoniovitordematos01@gmail.com>
2026-06-04 10:27:35 -0300

    feat: adciona estrutura de dados do sistema

---
+++ dados.py
@@ -0,0 +1,8 @@
+# Tipos de combustível (tupla — valores fixos)
+tipos_combustivel = ("Gasolina Comum", "Gasolina Aditivada", "Etanol", "Diesel")
+
+# Lista onde os abastecimentos serão guardados
+abastecimentos = []
+
+# Set para evitar CPFs duplicados
+cpfs_cadastrados = set()
